f1f3daa01a5a41232371b8129b67a35f0b93ffdb
kurniawidis <55923784+kurniawidis@users.noreply.github.com>
2020-03-25 10:33:08 -0700

    Add files via upload

---
+++ database/db.php
@@ -0,0 +1,4 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    $con = mysqli_connect("localhost","id12794722_kurniawidis","tisna123","id12794722_akademik") or die ("could not connect database");
+?>
